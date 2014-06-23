e9188ddcd19326769edb37bb7402c59bc9572b57
Kedar Warriner <kedar@cpan.org>
2014-06-23 17:51:57 +0000

    Import of KEDAR/Device-Neurio-0.03 from CPAN.
    gitpan-cpan-distribution: Device-Neurio
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         KEDAR/Device-Neurio-0.03.tar.gz
    gitpan-cpan-author:       KEDAR
    gitpan-cpan-maturity:     released

--- lib/Device/Neurio.pm
+++ lib/Device/Neurio.pm
@@ -1,4 +1,4 @@
-package Device::Neurio;
+package Neurio;
 
 use warnings;
 use strict;
@@ -22,11 +22,11 @@ Device::Neurio - Methods for accessing data collected by a Neurio sensor module.
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 #*****************************************************************
 
